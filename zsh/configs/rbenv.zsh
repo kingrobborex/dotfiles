3cd367307401ed54467b783a82ee223265dfc6da
Robert Forbes <robert@roboticdinosaur.co.uk>
2016-09-10 16:43:05 +0100

    install rbenv and phpenv if not detected in local folders...

--- zsh/configs/rbenv.zsh
+++ zsh/configs/rbenv.zsh
@@ -1,2 +1,15 @@
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
+ZSH_RBENV_DIR=${0:a:h}
+
+[[ -z "$RBENV_DIR" ]] && export RBENV_DIR="$HOME/.rbenv"
+
+_zsh_rbenv_install() {
+  echo "Installing rbenv..."
+  git clone https://github.com/rbenv/rbenv.git "$RBENV_DIR"
+}
+
+[[ ! -d "$RBENV_DIR" ]] && _zsh_rbenv_install
+
+if [[ -d "$RBENV_DIR" ]]; then
+  export PATH="$HOME/.rbenv/bin:$PATH"
+  eval "$(rbenv init -)"
+fi
