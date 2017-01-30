a3cfb2037d48e2381d615b6cf38ea88f0298c6e4
Robert Forbes <robert@roboticdinosaur.co.uk>
2017-01-30 01:48:03 +0000

    Fix variable to install plugins properly

--- zsh/configs/rbenv.zsh
+++ zsh/configs/rbenv.zsh
@@ -7,10 +7,10 @@ _zsh_rbenv_install() {
   git clone https://github.com/rbenv/rbenv.git "$RBENV_DIR"
 
   echo "Installing ruby-build..."
-  git clone https://github.com/rbenv/ruby-build.git $(rbenv root)/plugins/ruby-build
+  git clone https://github.com/rbenv/ruby-build.git "$RBENV_DIR"/plugins/ruby-build
 
   echo "Installing rbenv-default-gems..."
-  git clone https://github.com/rbenv/rbenv-default-gems.git $(rbenv root)/plugins/rbenv-default-gems
+  git clone https://github.com/rbenv/rbenv-default-gems.git "$RBENV_DIR"/plugins/rbenv-default-gems
 }
 
 [[ ! -d "$RBENV_DIR" ]] && _zsh_rbenv_install
