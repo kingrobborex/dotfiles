2cec8adaafc77cc6d10d11f2b791721be5f80642
Robert Forbes <robert@roboticdinosaur.co.uk>
2019-07-30 15:34:58 +0100

    updates

--- zsh/configs/go.zsh
+++ zsh/configs/go.zsh
@@ -1,5 +1,5 @@
 # Load Gopath
-export GOPATH="$HOME/go"
-export PATH="$PATH:$GOPATH/bin"
+#export GOPATH="$HOME/go"
+#export PATH="$PATH:$GOPATH/bin"
 
 command -v vg >/dev/null 2>&1 && eval "$(vg eval --shell zsh)"
