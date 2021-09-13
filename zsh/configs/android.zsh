5fc4ec69345cb97749c2049cce1999fe99e6bc53
Robert Forbes <robert@roboticdinosaur.co.uk>
2021-09-13 20:26:24 +0100

    Update paths to suit Ubuntu

--- zsh/configs/android.zsh
+++ zsh/configs/android.zsh
@@ -7,32 +7,7 @@ fi
 # Minimum Android platform based on:
 #
 # http://gs.statcounter.com/os-version-market-share/android/mobile-tablet/worldwide
-if [ -z "${ANDROID_MINIMUM_PLATFORM}" ]; then
-    export ANDROID_MINIMUM_PLATFORM=24
-fi
-
-if [ -z "${ANDROID_HOME}" ]; then
-    export ANDROID_HOME=/opt/android-sdk
-fi
-
-if [ -z "${ANDROID_NDK_HOME}" ]; then
-    export ANDROID_NDK_HOME=/opt/android-ndk
-fi
-
-get_last() {
-    ls $1 | sort -V | tail -n 1
-}
-
-if [ -z "${ANDROID_BUILD_TOOLS_REVISION}" ]; then
-    export ANDROID_BUILD_TOOLS_REVISION=$(get_last ${ANDROID_HOME}/build-tools)
-fi
-
-if [ -z "${ANDROID_API_VERSION}" ]; then
-    export ANDROID_API_VERSION=android-$ANDROID_MINIMUM_PLATFORM
-fi
-
-if [ -z "${ANDROID_NDK_PLATFORM}" ]; then
-    export ANDROID_NDK_PLATFORM=android-$ANDROID_MINIMUM_PLATFORM
-fi
-
-export PATH=$PATH:$ANDROID_HOME/emulator:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/platform-tools
+export ANDROID_HOME=$HOME/Android/Sdk
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/emulator
